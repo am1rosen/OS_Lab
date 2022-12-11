bd789784ea79537b2b751470e960e22e28920b61
Amirhossein Hajizadeh <57001422+am1rosen@users.noreply.github.com>
2022-12-11 13:57:37 +0330

    Add files via upload

--- HW6-forks/q3.sh
+++ HW6-forks/q3.sh
@@ -10,44 +10,50 @@ recursiveSearch(){
 	for filedir in $var;
 	do
 		if [[ -f "$filedir" ]]; then
-			mkdir -p "$catPath/${filedir##*.}Files"
-			if [[ -f "$catPath/${filedir##*.}Files/$filedir" ]]; then
-				filename=$(basename -- "$filedir")
-				ext="${filename##*.}"
-				new="${filename%.*}"
-        	    		if [[ ! ${new: -1} == [0-9] ]]; then
-        	    			new=$new"1"
-        	    		fi
-        	    		while true;
-        	    		do
-        	    			if [[ -f "$catPath/${filedir##*.}Files/$new.$ext" ]]; then
-        	    				last=${new: -1}
-        	    				if [[ $last -eq 9 ]]; then
-        	    					if [[ ${new: -2: 1} == [1-9] ]]; then
-        	    						if [[ ${new: -3: 1} == [1-9] ]]; then
-        	    							last=${new: -3}
-        	    							last=$((last+1))
-        	    							new="${new::-3}$last"
+		
+			fileName=$(basename -- "$filedir")
+			extension="${fileName##*.}"
+			if [[ ! "$(pwd)" == "/home/am1rosen/Desktop/Categorized/"$extension"Files" ]]; then	# checking if not sorted before
+			
+				mkdir -p "$catPath/${filedir##*.}Files"
+				if [[ -f "$catPath/${filedir##*.}Files/$filedir" ]]; then
+					filename=$(basename -- "$filedir")
+					ext="${filename##*.}"
+					new="${filename%.*}"
+        	    			if [[ ! ${new: -1} == [0-9] ]]; then
+        	    				new=$new"1"
+        	    			fi
+        	    			while true;
+        	    			do
+        	    				if [[ -f "$catPath/${filedir##*.}Files/$new.$ext" ]]; then
+        	    					last=${new: -1}
+        	    					if [[ $last -eq 9 ]]; then
+        	    						if [[ ${new: -2: 1} == [1-9] ]]; then
+        	    							if [[ ${new: -3: 1} == [1-9] ]]; then
+        	    								last=${new: -3}
+        	    								last=$((last+1))
+        	    								new="${new::-3}$last"
+        	    							else
+        	    								last=${new: -2}
+        	    								last=$((last+1))
+        	    								new="${new::-2}$last"
+        	    							fi
         	    						else
-        	    							last=${new: -2}
         	    							last=$((last+1))
-        	    							new="${new::-2}$last"
+        	    							new="${new::-1}$last"
         	    						fi
         	    					else
         	    						last=$((last+1))
         	    						new="${new::-1}$last"
         	    					fi
-        	    				else
-        	    					last=$((last+1))
-        	    					new="${new::-1}$last"
+						else
+							mv -n "$filedir" "$catPath/${filedir##*.}Files/$new.${filedir##*.}"
+							break
         	    				fi
-					else
-						mv -n "$filedir" "$catPath/${filedir##*.}Files/$new.${filedir##*.}"
-						break
-        	    			fi
-        	    		done
-        	    	else
-        	    		mv -n "$filedir" "$catPath/${filedir##*.}Files"
+        	    			done
+        	    		else
+        	    			mv -n "$filedir" "$catPath/${filedir##*.}Files"
+        	    	fi
 	        fi
 		else
         		if [[ -d "$filedir" ]]; then
@@ -62,6 +68,12 @@ recursiveSearch(){
 }
 
 
+
+
+
+
 cd ..
 cd ..
 recursiveSearch
+
+find /home/am1rosen/Desktop/Categorized -empty -type d -delete
